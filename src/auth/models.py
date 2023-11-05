139968db7f35372081ad5c56549d7302065f169b
askhat <ashimov.ashat05@gmail.com>
2023-11-05 18:34:24 +0600

    User table changes

--- src/auth/models.py
+++ src/auth/models.py
@@ -17,8 +17,7 @@ user = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
-    Column("name", String, nullable=False),
-    Column("surname", String, nullable=False),
+    Column("phone_number", String, nullable=False),
     Column("username", String, nullable=False),
     Column("hashed_password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
@@ -27,3 +26,24 @@ user = Table(
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False)
 )
+
+address = Table(
+    "address",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("unit_number", String),
+    Column("street_number", String, nullable=False),
+    Column("address_line1", String, nullable=False),
+    Column("address_line2", String, nullable=False),
+    Column("city", String, nullable=False),
+    Column("region", String, nullable=False),
+    Column("postal_code", String, nullable=False)
+)
+
+user_address = Table(
+    "user_address",
+    metadata,
+    Column("user_id", Integer, ForeignKey(user.c.id)),
+    Column("address_id", Integer, ForeignKey(address.c.id))
+)
+
