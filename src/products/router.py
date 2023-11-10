79119378de16079303b9bf5456b7fa8481e13251
askhat <ashimov.ashat05@gmail.com>
2023-11-11 00:21:33 +0600

    products router and schemas

--- src/products/router.py
+++ src/products/router.py
@@ -1,26 +1,41 @@
 from fastapi import APIRouter, Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.utils import get_user_db
-from src.products.schemas import ProductSchema
+from src.database import get_async_session
+from src.products.models import product_item, product
+from src.products.schemas import ProductSchema, ProductItemSchema
 
 router = APIRouter(
     prefix="/products",
     tags=["product"]
 )
 
-database = []
+
+@router.post("/")
+async def create_product(new_product: ProductSchema, new_product_item: ProductItemSchema, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(product).values(**new_product.dict())
+    stmt_item = insert(product_item).values(**new_product_item.dict())
+    await session.execute(stmt)
+    await session.commit()
+    await session.execute(stmt_item)
+    await session.commit()
+    return {"status": 200, "new_product": new_product, "new_product_item": new_product_item}
 
 
-@router.post("/create", response_model=ProductSchema)
-async def create_product(product: ProductSchema, user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
-    current_user = await user_db.get_by_id(1)
-    product_info = product.dict()
-    product_info["user_id"] = current_user.id
-    database.append(product_info)
-    return product
+# @router.post("/")
+# async def create_product_item(new_product_item: ProductItemSchema, session: AsyncSession = Depends(get_async_session)):
+#     try:
+#         stmt = insert(product_item).values(**new_product_item.dict())
+#         await session.execute(stmt)
+#         return new_product_item
+#     except Exception as error:
+#         return error
 
 
 @router.get("/{product_id}", response_model=ProductSchema)
-async def get_product(product_id: int):
-    return database[product_id - 1]
+async def get_product(product_id: int,session: AsyncSession = Depends(get_async_session)):
+    stmt = select(product_item).where(product_item.c.id == product_id)
+    await session.execute(stmt)
--- src/products/schemas.py
+++ src/products/schemas.py
@@ -2,8 +2,16 @@ from pydantic import BaseModel
 
 
 class ProductSchema(BaseModel):
+    category_id: int
     name: str
-    quantity: str
     description: str
     product_image: str
+
+
+class ProductItemSchema(BaseModel):
+    product_id: int
+    SKU: int
+    qty_in_stock: int
+    product_image: str
+    price: int
 
