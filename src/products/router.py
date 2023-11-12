ec665096401f143db8c188a22e5d0ffeda7112c6
askhat <ashimov.ashat05@gmail.com>
2023-11-13 00:08:34 +0600

    routers changes

--- src/products/router.py
+++ src/products/router.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,10 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
 from src.products.models import product_item, product
 
-from src.products.productscheme import ProductSchema, ProductItemSchema
-
 from src.products.schemas import ProductSchema, ProductItemSchema
-
 
 
 router = APIRouter(
@@ -19,42 +14,63 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/{product_id}")
 async def get_product(product_id: int, session: AsyncSession = Depends(get_async_session)):
     try:
         stmt = select(product_item).where(product_item.c.id == product_id)
         result = await session.execute(stmt)
+        product_data = result.fetchone()
         return {
             "status": "success",
-            "data": result.all(),
+            "data": product_data,
             "details": None
         }
     except Exception:
         raise HTTPException(status_code=500, detail={
             "status": "error",
             "data": None,
-            "details": "z ndj. vfnm t,fk"
+            "details": "Error"
         })
 
 
 @router.post("/create_product")
 async def create_product(new_product: ProductSchema, new_product_item: ProductItemSchema, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(product).values(**new_product.dict())
-    stmt_item = insert(product_item).values(**new_product_item.dict())
-    await session.execute(stmt)
-    await session.commit()
-    await session.execute(stmt_item)
-    await session.commit()
-    return {"status": 200, "new_product": new_product, "new_product_item": new_product_item}
+    try:
+        stmt = insert(product).values(**new_product.dict())
+        stmt_item = insert(product_item).values(**new_product_item.dict())
+        await session.execute(stmt)
+        await session.commit()
+        await session.execute(stmt_item)
+        await session.commit()
+        return {
+            "status": "success",
+            "data": [new_product, new_product_item],
+            "details": None
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+        })
 
 
 @router.post("/create_product_item")
 async def create_product_item(new_product_item: ProductItemSchema, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(product_item).values(**new_product_item.dict())
-    await session.execute(stmt)
-    await session.commit()
+    try:
+        stmt = insert(product_item).values(**new_product_item.dict())
+        await session.execute(stmt)
+        await session.commit()
+        return {
+            "status": "success",
+            "data": new_product_item,
+            "details": None
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None
+         })
 
-    return {"status": 200, "data": new_product_item}
-
-    return {"status": 200, "data": new_product_item}
 
