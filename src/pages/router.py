f7fbb9980383e08ec542d778e9591cc60ab97945
askhat <ashimov.ashat05@gmail.com>
2023-11-17 01:47:46 +0600

    Pages and router for them

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
+from src.products.router import get_all_products
 
 router = APIRouter(
     prefix="/pages",
@@ -8,6 +9,7 @@ router = APIRouter(
 )
 
 templates = Jinja2Templates(directory="src/templates")
+
 
 @router.get("/base")
 def get_base_page(request: Request):
@@ -17,4 +19,19 @@ def get_base_page(request: Request):
 @router.get("/login")
 def get_login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.get("/admin")
+def get_admin_page(request: Request):
+    return templates.TemplateResponse("admin.html", {"request": request})
+
+
+@router.get("/users")
+def get_users_page(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request})
+
+
+@router.get("/products")
+def get_products_page(request: Request, products=Depends(get_all_products)):
+    return templates.TemplateResponse("products.html", {"request": request, "products": products["data"]})
 
